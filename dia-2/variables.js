5c6b3655d40b584858f49abd9b5bef3f51fa298f
Isma1004 <iscorodriguez99@gmail.com>
2025-11-10 17:15:14 -0600

    Aprendiendo Variables 2.0

---
+++ dia-2/variables.js
@@ -0,0 +1,9 @@
+//Let deja modificar despues 
+//const no deja modificar después
+//var es muy antiguo y no es recomendable usarlo 
+let nombre="Ismael";
+const edad=21;
+let hobbies=["Escuchar musica","Bailar","Ver a mi noviecita"];
+let aprender=true;
+
+console.log("Hola soy ${nombre}");
